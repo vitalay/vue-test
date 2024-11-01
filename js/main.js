b112d490562c8548c738c633080dbc5d23cbae83
vitalay <vitalay.efimov@gmail.com>
2024-11-01 21:15:24 +0500

    1

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//счетчик
+const Counter = {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++
+    }, 1000)
+  }
+}
+
+//дата
+
+Vue.createApp(Counter).mount('#counter')
+
+const AttributeBinding = {
+  data() {
+    return {
+      message: 'Страница загружена ' + new Date().toLocaleString()
+    }
+  }
+}
+Vue.createApp(AttributeBinding).mount('#bind-attribute')
+
